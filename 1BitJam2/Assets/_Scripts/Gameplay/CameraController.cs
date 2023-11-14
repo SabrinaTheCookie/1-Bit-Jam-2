d48ee1b62d09e16ba81a84805e21286cf51d3a9b
Bree Cooke <sabrinalcooke@gmail.com>
2023-11-14 15:15:28 +1300

    Moved files

--- 1BitJam2/Assets/_Scripts/Gameplay/CameraController.cs
+++ 1BitJam2/Assets/_Scripts/Gameplay/CameraController.cs
@@ -42,6 +42,7 @@ public class CameraController : MonoBehaviour
         vCamNoise = vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
+
     [ContextMenu("RotateCamera")]
     public void RotateCamera() { RotateCamera(false);}
     public void RotateCamera(bool antiClockwise)
