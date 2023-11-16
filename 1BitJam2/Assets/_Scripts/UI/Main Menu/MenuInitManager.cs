be5cc0a40d9ab6bfda4141f6247a9ae25b63a931
Oliver Coates <oco36@uclive.ac.nz>
2023-11-16 19:54:52 +1300

    Redesigned the main menu. Added CRT draw line effect which looks kinda meh.

---
+++ 1BitJam2/Assets/_Scripts/UI/Main Menu/MenuInitManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInitManager : MonoBehaviour
+{
+    // Provides stage diretion for the main menu:
+    public float initialWaitTime = 1;
+    public ScreenDrawEffect screenDraw;
+
+    public CanvasGroup openingCanvas;
+    public CanvasGroup mainCanvas;
+
+    private bool finishedRenderingIntroduction = false;
+
+    public void Start()
+    {
+        StartCoroutine(GameOpening());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (finishedRenderingIntroduction)
+            {
+                StartCoroutine(ShowMainMenu());
+            }
+        }
+    }
+
+    public IEnumerator GameOpening()
+    {
+        openingCanvas.alpha = 1f;
+        openingCanvas.blocksRaycasts = true;
+
+        mainCanvas.alpha = 0f;
+        mainCanvas.blocksRaycasts = false;
+
+        yield return new WaitForSeconds(initialWaitTime);
+
+        screenDraw.StartScanDraw();
+
+        yield return new WaitForSeconds(2f); // Wait for the screen to render
+        finishedRenderingIntroduction = true;
+
+        // Wait for space input (bad bad bad code)
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return new WaitForEndOfFrame();
+            // :3
+        }
+        
+
+    }
+
+    public IEnumerator ShowMainMenu()
+    {
+        screenDraw.ResetScanLines();
+        screenDraw.StartScanDraw();
+
+        openingCanvas.alpha = 0f;
+        openingCanvas.blocksRaycasts = false;
+
+        mainCanvas.alpha = 1f;
+        mainCanvas.blocksRaycasts = true;
+
+        yield return new WaitForSeconds(initialWaitTime);
+    }
+}
